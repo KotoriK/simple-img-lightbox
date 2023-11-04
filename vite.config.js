5f11aa70dfc32535c6d9a3ca390acf5c23dc29ce
KotoriK <52659125+KotoriK@users.noreply.github.com>
2023-11-04 22:02:41 +0800

    chore

--- vite.config.js
+++ vite.config.js
@@ -18,7 +18,7 @@ export default defineConfig({
     },
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
-      formats: ['es', 'cjs'],
+      formats: ['es'],
       // the proper extensions will be added
       fileName: 'index'
     }
